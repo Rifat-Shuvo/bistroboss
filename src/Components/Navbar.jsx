8dcd65958d8fb8961bc556ccca1e3f3e29f0ccb1
Rifat-Shuvo <rhshuvo2812@gmail.com>
2023-11-17 18:51:36 +0600

    Home pages is desined started

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     </>
     return (
         <div>
-            <div className="navbar fixed z-10 bg-opacity-30 bg-black text-white">
+            <div className="navbar max-w-screen-xl fixed z-10 bg-opacity-30 bg-black text-white">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
