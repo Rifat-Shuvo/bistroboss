684c01afa65392523dd4568a5976a2d58c201faf
Rifat-Shuvo <rhshuvo2812@gmail.com>
2023-11-17 18:34:08 +0600

    Project started now and the all dependencies is installed

---
+++ src/Routes.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {createBrowserRouter} from "react-router-dom";
+import Root from './Layout/Root';
+import Home from './Pages/Home/Home';
+
+
+const Routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root></Root>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            }
+        ]
+    }
+    
+])
+export default Routes;
